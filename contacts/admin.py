9fe3b122ce0d758a2b7384e080d5418f5d849c49
Cybervaldemar <cybervaldemar@gmail.com>
2016-07-24 12:41:17 +0300

    Test

--- contacts/admin.py
+++ contacts/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import Contact, Student
+from .models import Contact, Student, StudentOne
 
 admin.site.register(Contact)
 admin.site.register(Student)
+admin.site.register(StudentOne)
